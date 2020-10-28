3ce2f8016694a8c4cbe8673e3c6d19a564daafe8
Simoniezi <73497522+Simoniezi@users.noreply.github.com>
2020-10-28 13:09:19 +0100

    Delete responsiveTopNav.js

--- New English Website/responsiveTopNav.js
+++
@@ -1,8 +0,0 @@
-function responsiveTopNav() {
-    var x = document.getElementById("TopNav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
